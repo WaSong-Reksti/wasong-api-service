4a1fb0a2984a9d65c73331047cd7f114d7031471
ZeevHaydar <nzhifhaidar@gmail.com>
2024-05-06 22:41:26 +0700

    update dikit

---
+++ src/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	// "errors"
+	// "github.com/gin-gonic/gin"
+	// "net/http"
+	"fmt"
+)
+
+type student struct {
+	ID		string
+	Name	string
+	About	string
+
+}
+
+/**
+user type 
+*/
+type user struct {
+	Username 	string
+	Password 	string
+	Email 		string
+	Type 		string
+}
+
+func main() {
+	var myUser user = user{"hakuna_matata", "123456", "hakuna@example.com", "student"}
+	fmt.Println(myUser);
+}	
